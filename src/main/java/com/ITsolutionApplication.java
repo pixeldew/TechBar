b1f5f05b7d52d8f30ec852e7acac6ff6f5e9fa20
naj sana <najsanno@gmail.com>
2026-07-05 21:46:50 +0530

    Users Data Code

---
+++ src/main/java/com/ITsolutionApplication.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ITsolutionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ITsolutionApplication.class, args);
+	}
+
+}
